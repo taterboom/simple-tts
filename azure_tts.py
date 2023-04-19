811d4c35ef14dc9ade1e9051b6854f18e34fb3c9
xueyong <1085523029@qq.com>
2023-04-19 18:19:39 +0800

    ✨ add azure api

---
+++ azure_tts.py
@@ -0,0 +1,55 @@
+import os
+import io
+import azure.cognitiveservices.speech as speechsdk
+import wave
+import time
+
+
+def datenow():
+    current_time = time.time()  # returns current Unix timestamp in seconds
+    # convert seconds to milliseconds
+    current_time_ms = int(round(current_time * 1000))
+    return current_time_ms
+
+
+def tts(text: str, speaker: str) -> io.BytesIO | None:
+    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get(
+        'SPEECH_KEY'), region=os.environ.get('SPEECH_REGION'))
+    speech_config.speech_synthesis_voice_name = speaker
+    speech_synthesizer = speechsdk.SpeechSynthesizer(
+        speech_config=speech_config, audio_config=None)
+    result = speech_synthesizer.speak_text_async(text).get()
+
+    if result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = result.cancellation_details
+        print("Speech synthesis canceled: {}".format(
+            cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            if cancellation_details.error_details:
+                print("Error details: {}".format(
+                    cancellation_details.error_details))
+        return None
+
+    stream = speechsdk.AudioDataStream(result)
+
+    # placehodler with "1"
+    audio = b'1'
+    audio_buffer = bytes(16000)
+    filled_size = stream.read_data(audio_buffer)
+
+    while filled_size > 0:
+        audio += audio_buffer[:filled_size]
+        filled_size = stream.read_data(audio_buffer)
+
+    file = io.BytesIO()
+    file.name = f'{datenow()}.wav'
+    with wave.open(file, 'wb') as output:  # Open temporary file as bytes
+        output.setparams((1, 2, 16000, 0, 'NONE', 'not compressed'))
+        output.writeframes(audio[1:])
+    file.seek(0)  # Rewind the file
+
+    return file
+
+
+if __name__ == '__main__':
+    print(tts("hello world", "en-US-JessaNeural"))
--- server.py
+++ server.py
@@ -1,16 +1,31 @@
 # flask server for the web app
 
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 
-from tts import tts
+from transformer_tts import tts as transformer_tts
+from azure_tts import tts as azure_tts
 
 app = Flask(__name__)
 
 
-@app.route('/tts/<text>/<speaker>')
-def tts_route(text, speaker):
-    b = tts(text, speaker)
-    return send_file(b, mimetype='audio/wav')
+@app.route('/tts', methods=['POST'])
+def route_tts():
+    params = request.get_json()
+    type = params['type']
+    text = params['text']
+    speaker = params['speaker']
+
+    if type == 'azure':
+        b = azure_tts(text, speaker)
+    elif type == 'transformer':
+        b = transformer_tts(text, speaker)
+    else:
+        return "error", 500
+
+    if b:
+        return b.getvalue().hex()
+    else:
+        return "error", 500
 
 
 if __name__ == '__main__':
